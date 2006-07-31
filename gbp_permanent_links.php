72184333d4bf2adea7f8db41607c03fa0ee3e5b4
graeme <graeme@rgbp.co.uk>
2006-07-31 17:05:04 +0000

    Added CSS and help sections.

--- gbp_permanent_links.php
+++ gbp_permanent_links.php
@@ -15,9 +15,29 @@ $plugin['revision'] = '$LastChangedRevision$';
 
 if (0) {
 ?>
-<!--
+<!-- CSS SECTION
+# --- BEGIN PLUGIN CSS ---
+<style type="text/css">
+div#permlink_help td { vertical-align:top; }
+div#permlink_help code { font-weight:bold; font: 105%/130% "Courier New", courier, monospace; background-color: #FFFFCC;}
+div#permlink_help code.code_tag { font-weight:normal; border:1px dotted #999; background-color: #f0e68c; display:block; margin:10px 10px 20px; padding:10px; }
+div#permlink_help a:link, div#l10n_help a:visited { color: blue; text-decoration: none; border-bottom: 1px solid blue; padding-bottom:1px;}
+div#permlink_help a:hover, div#l10n_help a:active { color: blue; text-decoration: none; border-bottom: 2px solid blue; padding-bottom:1px;}
+div#permlink_help h1 { color: #369; font: 20px Georgia, sans-serif; margin: 0; text-align: center; }
+div#permlink_help h2 { border-bottom: 1px solid black; padding:10px 0 0; color: #369; font: 17px Georgia, sans-serif; }
+div#permlink_help h3 { color: #693; font: bold 12px Arial, sans-serif; letter-spacing: 1px; margin: 10px 0 0;text-transform: uppercase;}
+</style>
+# --- END PLUGIN CSS ---
+-->
+<!-- HELP SECTION
 # --- BEGIN PLUGIN HELP ---
+<div id="adminlib_help">
+ 
+h1(#top). gbp_permanent_links.
 
+Provides custom, user defined, permanent links formats.
+
+</div>
 # --- END PLUGIN HELP ---
 -->
 <?php
