41561935d53e2d890b977113781c18d2adaaa71d
Graeme Porteous <graeme@rgbp.co.uk>
2008-12-14 18:51:35 +0000

    Set global css variable to the style for the current section

--- gbp_permanent_links.php
+++ gbp_permanent_links.php
@@ -579,7 +579,7 @@ class PermanentLinks extends GBPPlugin
 				}
 
 				// Export required values to the global namespace
-				foreach (array('id', 's', 'c', 'pg', 'is_article_list', 'prev_id', 'prev_title', 'next_id', 'next_title') as $key) {
+				foreach (array('id', 's', 'c', 'pg', 'is_article_list', 'prev_id', 'prev_title', 'next_id', 'next_title', 'css') as $key) {
 					if (array_key_exists($key, $pretext))
 						$GLOBALS[$key] = $pretext[$key];
 				}
